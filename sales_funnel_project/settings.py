5561739595bf947f460fb010ab2fb3ccdfb892d6
samukele-dev <samukele107@gmail.com>
2023-08-16 10:06:15 +0200

    collected staticfiles

--- sales_funnel_project/settings.py
+++ sales_funnel_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 
 # Default primary key field type
